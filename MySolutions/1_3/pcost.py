fc5f841a4eb9765c589293151a8a00d7d0e988ea
Nicolas <nicolascampos1093@gmail.com>
2023-10-29 11:45:56 -0300

    update to use modern python terminology

--- MySolutions/1_3/pcost.py
+++ MySolutions/1_3/pcost.py
@@ -1,13 +1,14 @@
 # pcost.py
 
 def calculate_stock_prices():
-    stocks = open('./Data/portfolio.dat', 'r').read().split()
-    total_price = 0.0
-    
-    for i in range(int(len(stocks)/3)):
-        number_of_shares = int(stocks[(i*3)+1])
-        stock_price = float(stocks[(i*3)+2])
-        total_price += number_of_shares * stock_price
+    with open('./Data/portfolio.dat', 'r') as f:
+        stocks = f.read().split()
+        total_price = 0.0
+        
+        for i in range(int(len(stocks)/3)):
+            number_of_shares = int(stocks[(i*3)+1])
+            stock_price = float(stocks[(i*3)+2])
+            total_price += number_of_shares * stock_price
     
     return total_price
 
